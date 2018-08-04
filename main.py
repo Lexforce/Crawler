0a49ffbb7b47d85ddac975989012a1b93b88e41a
Linus Lexfors <linuslexfors@hotmail.com>
2018-08-05 01:23:35 +0200

    API check

---
+++ main.py
@@ -0,0 +1,32 @@
+import urllib.request
+import re
+start_url = 'http://tedgreene.com'
+visited = []
+to_visit = []
+to_visit.append(start_url)
+
+
+while(len(to_visit) > 0):
+    current_url = to_visit.pop()
+    print("current_url =", current_url)
+    with urllib.request.urlopen(current_url) as response:
+        visited.append(current_url)
+        content = response.read().decode('utf-8', 'ignore')
+
+        #page = content.decode('UTF-8')
+        #print(content.type)
+        lines = content.split('\n')
+        print(len(lines))
+        for line in lines:
+            m = re.search('href="(\w*/\w*.asp)"', line)
+            m2 = re.search('href="(\w*/\w*.pdf)"', line)
+            if m:
+                #print(m.group(1))
+                new_url = current_url + '/' + m.group(1)
+                print(new_url)
+                if new_url not in visited:
+                    to_visit.append(new_url)
+            if m2:
+                print(start_url + '/' + m2.group(1))
+
+print("\n\n***to_visit empty***\n")
