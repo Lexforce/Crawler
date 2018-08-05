d0a38bb3a32d67fb21722a87d366995440e7261a
Linus Lexfors <linuslexfors@hotmail.com>
2018-08-05 21:19:05 +0200

    fooling around with regex

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import urllib.request
 import re
+
+
+
 start_url = 'http://tedgreene.com'
 visited = []
 to_visit = []
@@ -8,7 +11,7 @@ to_visit.append(start_url)
 
 while(len(to_visit) > 0):
     current_url = to_visit.pop()
-    print("current_url =", current_url)
+    print("current_url = {}\n".format(current_url))
     with urllib.request.urlopen(current_url) as response:
         visited.append(current_url)
         content = response.read().decode('utf-8', 'ignore')
@@ -17,11 +20,14 @@ while(len(to_visit) > 0):
         #print(content.type)
         lines = content.split('\n')
         #print(len(lines))
+
         for line in lines:
-            m = re.search('href="(\w*/\w*.asp)"', line)
+            m = re.search('<a href="([^"]*)">', line)
             m2 = re.search('href="(\w*/\w*.pdf)"', line)
             if m:
                 #print(m.group(1))
+                #last_slash = current_url.rfind('/')
+                print("matched group: " + m.group(1))
                 new_url = current_url + '/' + m.group(1)
                 if new_url not in visited:
                     print("new url: {}".format(new_url))
